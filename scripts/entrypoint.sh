933599d28efade92c1693a72a59a3324c9eff329
SamB <samuel.behan@dob.sk>
2020-09-16 22:10:17 +0200

    working gateway

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,7 @@
 case "$1" in
 	"--init" )
 		# START DANTED AS SERVICE
-		service ssh start;
+		service danted start;
 		tail -f /dev/null;
 		;;
 	"exec" )
@@ -15,9 +15,16 @@ case "$1" in
 		srv_port="";
 		if [ -n "$GATEWAY_REMOTE_HOST" ];
 		then
-			srv=`nslookup -querytype=srv -nofail -retry=2 "$GATEWAY_REMOTE_HOST." | grep -w "^$GATEWAY_REMOTE_HOST" | sort -k 5 -r | awk '{ print $7 ":" $6 }'`;
-			srv_host="${srv%:*}";
+			prefix="_socks._tcp";
+			srv=`nslookup -querytype=srv -nofail -retry=2 "$prefix.$GATEWAY_REMOTE_HOST." | grep -w "^$prefix.$GATEWAY_REMOTE_HOST" | sort -k 5 -r | awk '{ print $7 ":" $6 }' | grep -m 1 "^$prefix"`;
+			srv_user="${srv%:*}";
 			srv_port="${srv#*:}";
+
+			if [ -z "$srv" -o -z "$srv_user" -o -z "$srv_port" ];
+			then
+				echo "ERROR: Failed to discover forward configuration !" >&2;
+				exit 1;
+			fi;
 		else
 			echo "ERROR: Variable GATEWAY_REMOTE_HOST not defined !" >&2;
 			exit 1;
@@ -25,8 +32,12 @@ case "$1" in
 
 		# exec now
 		export GATEWAY_PORT="$srv_port";
-		exec ssh -o "RemoteForward=$GATEWAY_PORT" -R "$(($GATEWAY_PORT+1)):localhost:1080" "$srv_host@$GATEWAY_REMOTE_HOST" -- "$@";
+		exec ssh -o "RemoteForward=$GATEWAY_PORT" -R "$(($GATEWAY_PORT+1)):localhost:1080" "$srv_user@$GATEWAY_REMOTE_HOST" -- "$@";
 		;;
+	"" )
+		echo "COMMAND MISSING !" >&2;
+		exit 1;
+		;;		
 	* )
 		echo "UNSUPPORTED COMMAND: $1" >&2;
 		exit 1;
